1765d5b7cb31c2f57c192d16746c10ea849ff56b
insiyaa <insiyahhajoori@gmail.com>
2025-01-28 13:12:34 +0530

    Update gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Skophob`,
+    siteTitleAlt: `Skophob - Where learning is fun!`,
+    siteHeadline: `Explore a virtual academy and complete story-driven challenges to learn`,
+    siteUrl: `https://skophob.com`,
+    siteDescription: `explore a virtual academy and complete story-driven challenges to learn`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@insiyaa`,
   },
   trailingSlash: `always`,
   plugins: [
